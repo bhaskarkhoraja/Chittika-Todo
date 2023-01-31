7653cabf02da2fafec3ba5cd0773a5c5f2d71d0f
Bhaskar-Khoraja <bkhoraja@gmail.com>
2023-01-31 16:25:59 +0545

    wrapped with contextAPI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import TodoState from './context/TodoState'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TodoState>
+      <App />
+    </TodoState>
   </React.StrictMode>,
 )
