d51f3e842adb8cb616df1c9b26134e8870e549e1
Bhaskar-Khoraja <bkhoraja@gmail.com>
2023-02-01 10:01:55 +0545

    using useContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
+import React, { useContext, useEffect } from "react";
+import "./App.css";
+import Completed from "./components/Completed";
+import Navbar from "./components/Navbar";
+import Todo from "./components/Todo";
+import initialTodo from "./context/TodoContext";
 
 function App() {
+  const context = useContext(initialTodo);
+  const { todo, fetchTodo } = context;
+  // Get all Todo items from database
+  useEffect(() => {
+    fetchTodo();
+  }, []);
   return (
     <div className="App">
       <Navbar />
+      <Todo />
+      <hr />
+      <Completed />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
