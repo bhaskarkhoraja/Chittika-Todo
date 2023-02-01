4ad305e1c878ae1b4f0024d7a7ee3334bf25ca20
Bhaskar-Khoraja <bkhoraja@gmail.com>
2023-02-01 12:00:17 +0545

    added modal component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
 import React, { useContext, useEffect } from "react";
 import "./App.css";
 import Completed from "./components/Completed";
+import Modal from "./components/Modal";
 import Navbar from "./components/Navbar";
 import Todo from "./components/Todo";
 import initialTodo from "./context/TodoContext";
 
 function App() {
   const context = useContext(initialTodo);
-  const { todo, fetchTodo } = context;
+  const { fetchTodo, modal } = context;
   // Get all Todo items from database
   useEffect(() => {
     fetchTodo();
   }, []);
   return (
     <div className="App">
+      {modal && <Modal />}
       <Navbar />
       <Todo />
       <hr />
