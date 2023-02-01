ab284b8872e191ab8122cc56f4623b3a513846a2
Bhaskar-Khoraja <bkhoraja@gmail.com>
2023-02-01 10:15:14 +0545

    add toggle delete get todo done

---
+++ client/src/context/TodoState.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { API_URL } from "../api/Config";
+import TodoContext from "./TodoContext";
+import { getTodoItems } from "../api/GetTodoItems";
+import { addTodoItems } from "../api/AddTodoItems";
+import { deleteTodoItem } from "../api/DeleteTodoItems";
+import { updateTodoItems } from "../api/UpdateTodoItems";
+
+const TodoState = (props) => {
+  const [todo, setTodo] = useState([]);
+
+  // Fetch todo from api
+  const fetchTodo = async () => {
+    const response = await getTodoItems();
+    setTodo(response);
+  };
+
+  // Add todo item
+  const addTodo = async (title, description, priority) => {
+    const response = await addTodoItems(title, description, priority);
+    const newTodo = { ...todo };
+    newTodo.result.push(response.currentTodo);
+  };
+
+  // Delete todo item
+  const deleteTodo = async (id) => {
+    await deleteTodoItem(id);
+
+    function removeObjectWithId(arr, id) {
+      const objWithIdIndex = arr.findIndex((obj) => obj._id === id);
+
+      if (objWithIdIndex > -1) {
+        arr.splice(objWithIdIndex, 1);
+      }
+
+      return arr;
+    }
+    const newTodo = { ...todo };
+    newTodo.result = await removeObjectWithId(newTodo.result, id);
+    newTodo.count = newTodo.count - 1;
+    setTodo(newTodo);
+  };
+
+  const toggleTodo = async (index) => {
+    const newTodo = { ...todo };
+    newTodo.result[index].Completed = !newTodo.result[index].Completed;
+    setTodo(newTodo);
+    updateTodoItems(newTodo.result[index]);
+  };
+
+  return (
+    <TodoContext.Provider
+      value={{ todo, fetchTodo, addTodo, deleteTodo, toggleTodo }}
+    >
+      {props.children}
+    </TodoContext.Provider>
+  );
+};
+
+export default TodoState;
